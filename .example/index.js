665216316f3b72849b00d6d83c4f73fc51d9bf4c
kthjm <toxictoxer@gmail.com>
2018-07-12 11:53:47 +0900

    Add convert function

--- .example/index.js
+++ .example/index.js
@@ -1,9 +1,16 @@
-import compose from '..'
-import json from 'chin-plugin-json'
+import compose, { convert } from '..'
 import unified from 'chin-plugin-unified'
 import hast2mdast from 'rehype-remark'
 import mdast2hast from 'remark-rehype'
+import json from 'chin-plugin-json'
+import inkscape from 'chin-plugin-inkscape'
+import gulp from 'chin-plugin-gulp'
+import gulpImagemin from 'gulp-imagemin'
+import chinImagemin from 'chin-plugin-imagemin'
+
 import { join } from 'path'
+const put = join(__dirname, 'put')
+const out = join(__dirname, 'out')
 
 const md2html2json = compose([
   unified('m2h', [mdast2hast]),
@@ -15,12 +22,28 @@ const html2md2json = compose([
   json()
 ])
 
+const ink2min2png2min = compose([
+  gulp(() => [ gulpImagemin() ]),
+  convert('buffer2stream'),
+  inkscape('png'),
+  convert('stream2buffer'),
+  chinImagemin()
+])
+
+const ink2png2min = compose([
+  inkscape('png'),
+  convert('s2b'),
+  gulp(() => [ gulpImagemin() ]),
+  convert('b2s')
+])
+
 export default {
-  put: join(__dirname, 'put'),
-  out: join(__dirname, 'out'),
+  put,
+  out,
   clean: true,
-  processors: {
-    md: md2html2json,
-    html: html2md2json
-  }
+  processors: [
+    ['read_as_buffer', { svg: ink2min2png2min }],
+    ['read_as_stream', { svg: ink2png2min }],
+    ['*', { md: md2html2json, html: html2md2json }]
+  ]
 }
--- index.js
+++ index.js
@@ -1,49 +1,90 @@
 const { parse, format } = require('path')
+const { Transform, Readable } = require('stream')
 const { assign } = Object
 
-module.exports = (extensions) => ({
-  options: extensions[0].options,
-  isStream: extensions[0].isStream,
-  processor: (data, util) =>
-    recursiveProcess([].concat(extensions), data, util)
-})
+const NAME = 'chin-plugin-compose'
 
-const recursiveProcess = (extensions, data, util) =>
-  Promise.resolve()
-  .then(() =>
-    extensions
-    .splice(0, 1)[0]
-    .processor(data, util)
-  )
-  .then(result => resultNormalize(result, util.out))
-  .then(results =>
-    !extensions.length
-    ?
-    results
-    :
-    Promise.all(
-      results.map(([ outpath, processed ]) =>
-        recursiveProcess(
-          extensions,
-          processed,
-          assign({}, util, { out: parseXbase(outpath) })
-        )
+const throws = (message) => { throw new Error(message) }
+const asserts = (conditon, message) => !conditon && throws(message)
+const parseXbase = (path) => {
+  const { root, dir, name, ext } = parse(path)
+  return { root, dir, name, ext }
+}
+
+/* compose */
+const compose = (extensions) => {
+  asserts(Array.isArray(extensions), `(${NAME}) !Array.isArray(extensions)`)
+  return {
+    options: extensions[0].options,
+    isStream: extensions[0].isStream,
+    processor: (data_or_pipe, util) =>
+      reprocess(
+        [].concat(extensions),
+        data_or_pipe,
+        util
       )
-    )
-    .then(([ results ]) => results)
-  )
+  }
+}
 
-const resultNormalize = (result, out) =>
-  !Array.isArray(result) ?
-  [ [format(out), result] ] :
-
-  !Array.isArray(result[0]) ?
-  [ result ] :
-
+const result2results = (result, out) =>
+  !Array.isArray(result) ? [ [format(out), result] ] :
+  !Array.isArray(result[0]) ? [ result ] :
   result
 
-const parseXbase = (path) =>
-  Object
-  .entries(parse(path))
-  .filter(([key]) => key !== 'base')
-  .reduce((a, [key,value]) => assign(a, { [key]: value }), {})
+const reprocess = (extensions, data_or_pipe, util) =>
+  Promise.resolve()
+  .then(() => {
+    const extension = extensions.splice(0, 1)[0]
+    asserts(extension, `(${NAME}) !extension`)
+    asserts(typeof extension.processor === 'function', `(${NAME}) typeof extension.processor !== "function"`)
+    return extension.processor(data_or_pipe, util)
+  })
+  .then(result => {
+    const results = result2results(result, util.out)
+    const firstProcessed = results[0][1]
+    const resultIsStream = firstProcessed && typeof firstProcessed.pipe === 'function'
+
+    return !extensions.length
+    ? results
+    : Promise.all(results.map(([ outpath, processed ]) =>
+      reprocess(
+        extensions,
+        resultIsStream ? (...arg) => processed.pipe(...arg) : processed,
+        assign({}, util, { out: parseXbase(outpath) })
+      )
+    ))
+    .then(([ results ]) => results)
+  })
+
+
+/* convert */
+const convert = (type) =>
+  (type === 's2b' || type === 'stream2buffer') ? stream2buffer() :
+  (type === 'b2s' || type === 'buffer2stream') ? buffer2stream() :
+  throws(`(${NAME}) ${type} is invalid`)
+
+const stream2buffer = () => ({
+  isStream: true,
+  processor: (pipe, { out }) => new Promise((resolve, reject) => {
+    const buffers = []
+    const transform = new Transform({ transform(chunk, enc, cb) { cb(null, chunk) } })
+    const stream = pipe(transform)
+    stream.on('error', reject)
+    stream.on('data', (chunk) => buffers.push(chunk))
+    stream.on('end', () => resolve([ format(out), Buffer.concat(buffers) ]))
+  })
+})
+
+const buffer2stream = () => ({
+  isStream: false,
+  processor: (data, { out }) => {
+    const stream = new Readable()
+    stream.push(data)
+    stream.push(null)
+    return [ format(out), stream ]
+  }
+})
+
+module.exports = compose
+module.exports.compose = compose
+module.exports.convert = convert
